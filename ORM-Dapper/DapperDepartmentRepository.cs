b9edd839b3f72e2bff253500f1ab96b5c8d91df8
inkyourshoesgit config user.namegit config --global user.email inkyourshoes@gmail.comglobal user.name inkyourshoes <inkyourshoes@gmail.com>
2025-12-11 11:09:48 -0500

    ex1 redo start

---
+++ ORM-Dapper/DapperDepartmentRepository.cs
@@ -0,0 +1,26 @@
+using System.Data;
+using Dapper;
+
+
+namespace ORM_Dapper;
+
+public class DapperDepartmentRepository : IDepartmentRepository
+{
+    private readonly IDbConnection _connection;
+    public DapperDepartmentRepository(IDbConnection connection)
+    {
+        _connection = connection;
+    }
+
+    public IEnumerable<Department> GetAllDepartments()
+    {
+        return _connection.Query<Department>("SELECT * FROM Departments;");
+    }
+    
+    public void InsertDepartment(string newDepartmentName)
+    {
+        _connection.Execute("INSERT INTO Departments (Name) VALUES (@departmentName);", 
+            new { departmentName = newDepartmentName });
+    }
+}
+
---
+++ ORM-Dapper/Department.cs
@@ -0,0 +1,12 @@
+namespace ORM_Dapper;
+
+public class Department
+{
+    public Department()
+    {
+    }
+    
+    public int DepartmentId { get; set; }
+    public string Name { get; set; } = string.Empty;
+
+}
